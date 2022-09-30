201d9ad4c9e34cc06ceee59d203068e59e691528
Mikhail <mikhail@mikhail.com>
2022-09-30 22:17:08 +0200

    update

--- prob_calculator.py
+++ prob_calculator.py
@@ -12,40 +12,43 @@ class Hat:
         self.contents = contents
 
     def draw(self, number_of_balls):
+        if len(self.contents) < number_of_balls:
+            return self.contents
         draw_list = random.sample(self.contents, number_of_balls)
+        for ball in draw_list:
+            self.contents.remove(ball)
         return draw_list
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
     hat_contents = copy.copy(hat.contents)
+    if len(hat_contents) <= num_balls_drawn: return float(1)
+    total_balls_expect = sum(v for v in expected_balls.values())
     success = 0
+    mat = 0
     for _ in range(num_experiments):
-        draw_list = hat.draw(num_balls_drawn)
-        for ball in draw_list:
-            #print(ball)
-            #print(draw_list.count(ball))
-            #print(expected_balls.get(ball, 0))
-
-            #print(draw_list.count(ball) >= expected_balls.get(ball, 0))
-            if draw_list.count(ball) > expected_balls.get(ball, 0):
-                #print('-')
+        draw_list = random.sample(hat_contents, num_balls_drawn)
+        for ball, value in expected_balls.items():
+            if value > draw_list.count(ball):
                 continue
-            success += 1
+            mat += value
+        if mat == total_balls_expect: success += 1
+        mat = 0
     probability = success / num_experiments
     return probability
 
 
 print()
 print()
-#hat = Hat(black=6, red=4, green=3)
-#probability = experiment(hat=hat,
-#                  expected_balls={"red":2,"green":1},
-#                  num_balls_drawn=5,
-#                  num_experiments=2000)
-
-hat = Hat(black=1, red=1, green=1)
+hat = Hat(blue=3,red=2,green=6)
 probability = experiment(hat=hat,
-                  expected_balls={"red":1},
-                  num_balls_drawn=1,
-                  num_experiments=2000)
+                  expected_balls={"blue":2,"green":1},
+                  num_balls_drawn=44,
+                  num_experiments=5)
+
+#hat = Hat(black=1, red=1, green=1)
+#probability = experiment(hat=hat,
+                  #expected_balls={"red":1},
+                  #num_balls_drawn=1,
+                  #num_experiments=2000)
 
 print(probability)
