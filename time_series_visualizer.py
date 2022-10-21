95de3b6bcf2e17dccb9a13a895662ad8451016ee
Mikhail D <fsd.mikhail@gmail.com>
2022-10-21 21:31:48 +0300

    time_series done

--- time_series_visualizer.py
+++ time_series_visualizer.py
@@ -8,13 +8,10 @@ register_matplotlib_converters()
 df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates=['date'], index_col='date')
 not_in_first_2_5 = df['value'] >= df['value'].quantile(0.025)
 not_in_last_2_5 = df['value'] <= df['value'].quantile(0.975)
+
 # Clean data
 df = df[not_in_first_2_5 & not_in_last_2_5]
 
-#group_names = ['sdf', 'sdfsd', 'sdfsdf']
-#group_data = [1,2,3]
-#fig, ax = plt.subplots(figsize=(8, 8))
-#ax.barh(group_names, group_data)
 
 def draw_line_plot():
     # Draw line plot
@@ -29,49 +26,49 @@ def draw_line_plot():
     fig.savefig('line_plot.png')
     return fig 
 
+
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
     df_bar = df.resample('M').mean()
-    print(type(df_bar))
-    print(df.info())
-    print(df_bar.head())
+    df_bar['Month'] = df_bar.index.month_name()
+
+    month_list = pd.date_range('2000', periods=12, freq='M').month_name()
+
     # Draw bar plot
-
-    dada ={'first': ['a', 'b', 'c'], 'second': [1,2,3]}
-    df_dada = pd.DataFrame(dada)
-    sns.catplot(data=df_dada, x='first', y='second', kind='bar')  # it works
-    df_bar.reset_index(inplace=True)
-    print(df_bar.head())
-    sns.catplot(data=df_bar, x='date', y='value', kind='bar') 
-
-    #plt.bar(df_bar.index, df_bar['value'], width=8)
-    #sns.catplot(df_bar, kind='bar')
-
-    #sns.barplot(df_bar, x=df_bar.index, y=df_bar['value']) 
-    plt.gcf().autofmt_xdate()
-    plt.show()
-        
-
-
+    plt.figure(figsize=(8,8))
+    g = sns.barplot(x=df_bar.index.year, y=df_bar.value, hue=df_bar['Month'], hue_order=month_list, edgecolor='white')
+    plt.xticks(rotation='vertical')
+    plt.xlabel('Years') 
+    plt.ylabel('Average Page Views')
+    plt.legend(loc='upper left', title='Month')
+    plt.tight_layout()
     # Save image and return fig (don't change this part)
+    fig = g.figure 
     fig.savefig('bar_plot.png')
     return fig
+
 
 def draw_box_plot():
     # Prepare data for box plots (this part is done!)
     df_box = df.copy()
     df_box.reset_index(inplace=True)
-    df_box['year'] = [d.year for d in df_box.date]
-    df_box['month'] = [d.strftime('%b') for d in df_box.date]
+    df_box['Year'] = [d.year for d in df_box.date]
+    df_box['Month'] = [d.strftime('%b') for d in df_box.date]
+    df_box.rename(columns={'value': 'Page Views'}, inplace=True)
 
     # Draw box plots (using Seaborn)
+    month_list = pd.date_range('2000', periods=12, freq='M').month_name().str[:3]
+    
+    fig, (ax1, ax2) = plt.subplots(1,2, figsize=(18,6))
+    sns.boxplot(data=df_box, x='Year', y='Page Views', ax=ax1, fliersize=2)
+    sns.boxplot(data=df_box, x='Month', y='Page Views', ax=ax2, order=month_list, fliersize=2)
+    ax1.set_title('Year-wise Box Plot (Trend)')
+    ax2.set_title('Month-wise Box Plot (Seasonality)')
 
-
-
-
+    plt.tight_layout()
 
     # Save image and return fig (don't change this part)
     fig.savefig('box_plot.png')
     return fig
 
-draw_bar_plot()
+draw_box_plot()
